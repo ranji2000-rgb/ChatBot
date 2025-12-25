761013cbab9ded45c401d184917f746c67cd562d
ranji2000-rgb <ranji2000@gmail.com>
2025-12-25 12:26:54 +0530

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,11 @@
+NSE_BASE_URL = "https://www.nseindia.com"
+OPTION_CHAIN_API = "/api/option-chain-equities?symbol="
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br"
+}
+
+REQUEST_TIMEOUT = 10
+CACHE_TTL = 15
